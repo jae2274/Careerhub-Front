45812608f734824a7d4f9fc4d50e6ce0f9b7bbdb
jyo <jyo.liar@gmail.com>
2024-03-28 20:49:03 +0900

    채용공고상세페이지에도 accessToken을 전달하도록 추가

--- src/job_posting/postingId/api.js
+++ src/job_posting/postingId/api.js
@@ -1,7 +1,24 @@
 import {backendUrl} from "~/const";
+import {getGrantType, getAccessToken} from "~/jwt";
+
+function setAccessToken(header = {}) {
+  const grantType = getGrantType();
+  const accessToken = getAccessToken();
+  if (grantType && accessToken) {
+    header.Authorization = `${grantType} ${accessToken}`;
+  }
+  return header;
+}
+
+function checkHttpStatus(res) {
+  if (res.status === 401) {
+    window.location.href = loginUrl;
+  }
+}
 
 export async function getPostingDetail(site, postingId) {
+  const headers = setAccessToken();
   return await (
-    await fetch(`${backendUrl}/job_postings/${site}/${postingId}`)
+    await fetch(`${backendUrl}/job_postings/${site}/${postingId}`, {headers})
   ).json();
 }
