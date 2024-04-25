eabdfaee50c8dd08173ff946a3c0f6fede224b99
jyo <jyo.liar@gmail.com>
2024-04-25 14:17:13 +0900

    대문자로 잘못 명명된 파일 이름 수정

---
+++ src/view/login/SignIn.svelte
@@ -0,0 +1,15 @@
+<script>
+  export let authCodeUrls;
+</script>
+
+<div class="sc-449546b7-9 jzmuak">
+  {#each authCodeUrls as authCodeUrl}
+    <a href={authCodeUrl.url} target="_blank" rel="opener">
+      <button type="button"
+        ><span style="display:flex"
+          ><span class="blind">{authCodeUrl.authServer} 로그인</span></span
+        ></button
+      >
+    </a>
+  {/each}
+</div>
