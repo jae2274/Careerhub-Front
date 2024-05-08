23f92b50e9197d2e0d8bc16db06b86fb60eb27e9
jyo <jyo.liar@gmail.com>
2024-05-08 19:12:48 +0900

    태그별 스크랩공고목록 기능 추가

--- src/view/my/scrap-job/api.js
+++ src/view/my/scrap-job/api.js
@@ -1,9 +1,33 @@
 import {backendUrl} from "~/const";
 import {setAccessTokenToHeader, checkHttpStatus} from "~/httputils";
 
-export async function findScrapJobs() {
+export async function findScrapJobs(tag = null) {
   const headers = setAccessTokenToHeader();
-  const res = await fetch(`${backendUrl}/my/scrap-job`, {
+
+  const query = tag ? `?tag=${encodeURIComponent(tag)}` : "";
+
+  const res = await fetch(`${backendUrl}/my/scrap-job${query}`, {
+    headers,
+  });
+  checkHttpStatus(res);
+
+  return await res.json();
+}
+
+export async function findAllScrapTags() {
+  const headers = setAccessTokenToHeader();
+  const res = await fetch(`${backendUrl}/my/scrap-job/tags`, {
+    headers,
+  });
+  checkHttpStatus(res);
+
+  return await res.json();
+}
+
+export async function findUntaggedScrapJobs() {
+  const headers = setAccessTokenToHeader();
+
+  const res = await fetch(`${backendUrl}/my/scrap-job/untagged`, {
     headers,
   });
   checkHttpStatus(res);
--- src/view/my/scrap-job/index.svelte
+++ src/view/my/scrap-job/index.svelte
@@ -2,13 +2,50 @@
   import Layout from "~/components/my/Layout.svelte";
   import JobPostingList from "~/components/jobPostingList/JobPostingList.svelte";
 
-  import {findScrapJobs} from "~/view/my/scrap-job/api";
+  import {
+    findAllScrapTags,
+    findScrapJobs,
+    findUntaggedScrapJobs,
+  } from "~/view/my/scrap-job/api";
   let promises = [];
   promises = [...promises, findScrapJobs()];
+  const scrapTagsPromise = findAllScrapTags();
+
+  function findScrapJobsAction(tag) {
+    promises = [findScrapJobs(tag)];
+  }
+
+  function findUntaggedScrapJobsAction() {
+    promises = [findUntaggedScrapJobs()];
+  }
 </script>
 
 <Layout>
   <div slot="content">
+    <ul class="sc-e0579984-4 hkhKhq">
+      <li on:click={() => findScrapJobsAction()}>전체</li>
+      {#await scrapTagsPromise then scrapTags}
+        {#each scrapTags.tags as scrapTag}
+          <li on:click={() => findScrapJobsAction(scrapTag)}>
+            {scrapTag}
+          </li>
+        {/each}
+      {/await}
+      <li on:click={findUntaggedScrapJobsAction}>그 외</li>
+    </ul>
     <JobPostingList {promises}></JobPostingList>
   </div>
 </Layout>
+
+<style>
+  .hkhKhq {
+    display: flex;
+    margin-left: -20px;
+    list-style: none;
+  }
+  .hkhKhq > li {
+    position: relative;
+    padding: 0px 20px;
+    cursor: pointer;
+  }
+</style>
